6949741b52c6cc66929f270c9a88d5edb1a7ac92
Abel <abel.velasquez1997@gmail.com>
2024-11-07 11:53:58 -0500

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -134,7 +134,71 @@ class ApiController extends Controller
         curl_close($curl);
         return $response; 
     }
+    public function confirmacionContratoArrendamiento(Request $request){
+
+        $codigo = $request->input('codigo');
+        $numeroCelular = $request->input('celular');
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://graph.facebook.com/v19.0/107514592428270/messages',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'{
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": "+51'.$numeroCelular.'",
+            "type": "template",
+            "template": {
+                "name": "confirmacion_contrato_arrendamiento",
+                "language": {
+                    "code": "es_MX",
+                    "policy": "deterministic"
+                },
+                "components": [
+                    {
+                        "type": "body",
+                        "parameters": [
+                            {
+                                "type": "text", 
+                                "text": "'.$codigo.'"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "button",
+                        "sub_type": "url",
+                        "index": 0,
+                        "parameters": [
+                            {
+                                "type": "text",
+                                "text": "'.$codigo.'"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }',
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'Authorization: Bearer EAAMZCH1Vx020BO0O1nwg2kAYIZCeZCIz2hPlAZCuqikkPDu5Oc5OlLjnXLOBMiUUQpkkfxedzwVrYp381KyvWz9WMUei7WLe2ZBk1izM5lMidWPArjjG77rIjxpHkNuS78gdxUjDldwH0WFxZApi3sbFTbgT3XoyP6AbbV3Ek5Xs4sm6sP3ZCOsWblt0ijx6zTqoQXZC8gtlGhdbrZADi'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+    }
+
     public function notificacionSeguridadCiudadana(Request $request){
+        $this->servicioNotificacion('+51922355307','https://portal.munipuentepiedra.gob.pe/images/logo_muni_pp_white.png','abel','$fechaRegistro','$detalle','$latitud','$longitud','$url');
         $idInsidencia = $request->input('idInsidencia');
 
         $sql = DB::connection('sigtram_seguridad')->select('select 
@@ -151,7 +215,7 @@ class ApiController extends Controller
                                                         where i.iddb = '.$idInsidencia);
         foreach ($sql as $resultado) {
              //return $request;
-            $numeroCelular = '+51'.$resultado->telefonodb;
+            $numeroCelular = '+51922355307';
             $base64Image = $resultado->foto2;
             $serenazgo = $resultado->apellidosdb;
             $fechaRegistro = $resultado->fecha_registro;
